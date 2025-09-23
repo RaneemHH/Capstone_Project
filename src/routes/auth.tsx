88f18cb151f21f0ee9708ad24c6f4e1471cffa55
raneem hajhassan <alhajhassanraneem@gmail.com>
2025-09-23 11:32:36 +0000

    Revert "feat(auth): add login and register pages (pull request #1)"

--- src/routes/auth.tsx
+++
@@ -1,8 +0,0 @@
-import {LoginAuthentication} from "@/pages/login.tsx"
-import {RegisterAuthentication} from "@/pages/register.tsx"
-
-
-export const authRoutes = [
-    { path: "login", element: <LoginAuthentication /> },
-    { path: "register", element: <RegisterAuthentication /> }
-];
